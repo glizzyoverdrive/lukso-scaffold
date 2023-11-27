ac8b467e83e5ce42a90c807cfba567eca10584ee
0xstruct <0xstruct@gmail.com>
2023-11-27 10:56:48 +0630

    minor changes in UI for project name change

--- packages/nextjs/components/MetaHeader.tsx
+++ packages/nextjs/components/MetaHeader.tsx
@@ -14,8 +14,8 @@ type MetaHeaderProps = {
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/` : "/";
 
 export const MetaHeader = ({
-  title = "Scaffold-ETH 2 App",
-  description = "Built with 🏗 Scaffold-ETH 2",
+  title = "LUKSO Scaffold App",
+  description = "Built with LUKSO Scaffold 🛠️ and 🏗 Scaffold-ETH 2",
   image = "thumbnail.jpg",
   twitterCard = "summary_large_image",
   children,
---
+++ packages/nextjs/components/NavLink.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export const NavLink = ({ href, children }: { href: string; children: React.ReactNode }) => {
+  const router = useRouter();
+  const isActive = router.pathname === href;
+
+  return (
+    <Link
+      href={href}
+      passHref
+      className={`${
+        isActive ? "bg-secondary shadow-md" : ""
+      } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
+    >
+      {children}
+    </Link>
+  );
+};
